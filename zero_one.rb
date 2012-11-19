3e9af3e01cdf46b8fb2fbf26a220e0d411cbbab7
Kazuhiro Omura <doze.away.k@gmail.com>
2012-11-19 15:17:59 +0900

    Support one dart to finish case

--- zero_one.rb
+++ zero_one.rb
@@ -1,7 +1,18 @@
 class ZeroOne
   module Finisher
     def Finisher.finish?(rest)
-      true # Fake it
+      darts = []
+
+      (1..20).each do |i|
+        darts << ["S" + i.to_s, i]
+        darts << ["D" + i.to_s, i * 2]
+        darts << ["T" + i.to_s, i * 3]
+      end
+      darts << ["S-BULL", 50]
+      darts << ["D-BULL", 50]
+      darts << ["MISS", 0]
+
+      darts.map{|item| item.last }.include?(rest) # Is there any elegant implementation?
     end
   end
 end
@@ -9,8 +20,11 @@ end
 describe ZeroOne::Finisher do
   describe "rest 1 case" do
     subject { ZeroOne::Finisher.finish?(1) }
-    it { should eq(true) }
+    it { should be_true }
+  end
+
+  describe "rest 181 case" do
+    subject { ZeroOne::Finisher.finish?(181) }
+    it { should be_false }
   end
 end
-
-
